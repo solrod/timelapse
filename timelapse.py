b04c6d7096882e63b5ef6179213a24e7bcac91d9
solrod <40004932+solrod@users.noreplay.github.com>
2022-08-28 10:19:59 +0200

    Minor change to script end.

--- timelapse.py
+++ timelapse.py
@@ -4,9 +4,9 @@ from time import sleep
 
 
 # Variables
-previewTime = 3 # Preview to adjust camera in seconds
-numberPics = 12 # Number of pictures to be taken during timelapse.
-sleepTime = 2 # Break between pictures
+previewTime = 30 # Preview to adjust camera in seconds
+numberPics = 120 # Number of pictures to be taken during timelapse.
+sleepTime = 60 # Break between pictures
 resolution = (1024, 768) #(720, 480) (800, 600) (1024, 768)(1280, 720) (1280, 1024)(1600, 1200) (1920, 1080)
 frameDelay = 10 # Time between frames in 1/100 second, in gif file.
 picsFolder = '/home/pi/Desktop/timelapse/pics'
@@ -77,24 +77,3 @@ else:
     print('Ending')
 print('Scipt closing.')
 
-
-'''
-if ready.lower() == 'y':
-    
-    print('Starting timelapse.')
-    makeTimeLapse()
-    print('Finish making pictures.')
-    
-    fileFormat = input('Press "1" for gif format or "2" for mp4 format or "3" for both formats: ')
-    if fileFormat == '1':
-        makeGif()
-    elif fileFormat == '2':
-        makeMovie()
-    elif fileFormat == '3':
-        makeGif()
-        makeMovie()
-    else:
-        print('You didnt press 1 or 2 or 3')
-else:
-    print('Ending.')
-    '''
